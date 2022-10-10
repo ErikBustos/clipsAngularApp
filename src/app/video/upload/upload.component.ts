2c2b2cea32402cbc6b5cff0011d53c076f4f63b3
ErikBustos <erikbustosmo@gmail.com>
2022-10-10 13:35:02 -0500

    uploading clips template changes and storing in the db

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ClipService } from 'src/app/services/clip.service';
+import firebase from 'firebase/compat/app';
+import { Router } from '@angular/router';
+
+const uploadingMsg = 'Please wait! your clip is being uploaded';
+const successUploadMsg = 'Success! Your clip is now ready to share with the world.';
+const errorUploadMsg = 'Upload failed! Please try again later.';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
-export class UploadComponent implements OnInit {
+export class UploadComponent implements OnDestroy {
   isDragover = false;
   file: File | null = null;
   nextStep = false;
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = uploadingMsg;
+  inSubmission = false;
+  percentage = 0;
+  showPercentage = false;
+  user : firebase.User | null = null;
+  task? : AngularFireUploadTask;
 
   title = new FormControl('', {
     validators: [
@@ -23,15 +42,25 @@ export class UploadComponent implements OnInit {
     title: this.title
   });
 
-  constructor() { }
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth,
+    private clipsService: ClipService,
+    private router: Router
+  ) {
+    auth.user.subscribe(user => this.user = user)
+   }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    this.task?.cancel();
   }
 
   storeFile($event: Event) {
     this.isDragover = false;
 
-    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    this.file = ($event as DragEvent).dataTransfer ?
+      ($event as DragEvent).dataTransfer?.files.item(0) ?? null :
+      ($event.target as HTMLInputElement).files?.item(0) ?? null;
 
     if(!this.file || this.file.type !== 'video/mp4') {
       return ;
@@ -44,7 +73,60 @@ export class UploadComponent implements OnInit {
   }
 
   uploadFile() {
-    console.log('File Uploaded');
+    this.uploadForm.disable();
+
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = uploadingMsg;
+    this.inSubmission = true;
+    this.showPercentage = true;
+
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+
+    this.task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
+
+    this.task.percentageChanges().subscribe(progress => {
+      this.percentage = progress as number / 100;
+    })
+
+    this.task.snapshotChanges().pipe(
+      last(),
+      switchMap(() => clipRef.getDownloadURL())
+    ).subscribe({
+      next: async (url) => {
+        const clip = {
+          uid : this.user?.uid as string,
+          displayName : this.user?.displayName as string,
+          title: this.title.value,
+          fileName: `${clipFileName}.mp4`,
+          url,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        };
+
+        const clipDocRef= await this.clipsService.createClip(clip);
+
+        this.alertColor = 'green';
+        this.alertMsg = successUploadMsg;
+        this.showPercentage = false;
+
+        setTimeout(() => {
+          this.router.navigate([
+            'clip', clipDocRef.id
+          ])
+        }, 1000);
+      },
+      error: (err) => {
+        this.uploadForm.enable();
+
+        this.alertColor = 'red';
+        this.alertMsg = errorUploadMsg;
+        this.inSubmission = true;
+        this.showPercentage = false;
+        console.error(err);
+      }
+    })
   }
 
 }
