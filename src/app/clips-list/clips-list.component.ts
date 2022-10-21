ca4e5f475bc4b8b8fab5c57e01e798b4ac0baaef
ErikBustos <erikbustosmo@gmail.com>
2022-10-21 12:43:25 -0500

    implemented cross-origin for gcloud, lazyLoading video module to improve performance, added videoJs to video Player

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { ClipService } from '../services/clip.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
-  styleUrls: ['./clips-list.component.css']
+  styleUrls: ['./clips-list.component.css'],
+  providers: [DatePipe]
 })
-export class ClipsListComponent implements OnInit {
+export class ClipsListComponent implements OnInit, OnDestroy {
+  @Input() scrollable = true;
 
-  constructor() { }
+  constructor(public clipService: ClipService) {
+    this.clipService.getClips();
+  }
 
   ngOnInit(): void {
-    window.addEventListener('scroll', this.handleScroll);
+    if(this.scrollable) {
+      window.addEventListener('scroll', this.handleScroll)
+    }
   }
 
   ngOnDestroy() {
-    window.removeEventListener('scroll', this.handleScroll);
+    if(this.scrollable) {
+      window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    this.clipService.pageClips = []
   }
 
   handleScroll = () => {
@@ -24,7 +36,7 @@ export class ClipsListComponent implements OnInit {
     const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
 
     if(bottomOfWindow) {
-      console.log('bottom of window');
+      this.clipService.getClips()
     }
 
     
